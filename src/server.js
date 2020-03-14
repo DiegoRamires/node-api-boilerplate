85402dac01ba01160ba065f4154fb7c6de8d31e8
DiegoRamires <goncalves.ramires@gmail.com>
2020-03-14 20:08:40 -0300

    refactoring app. create server and routes as indepent files

--- src/server.js
+++ src/server.js
@@ -1,12 +1,7 @@
-const express = require("express");
-const morgan = require("morgan");
-
-const server = express();
+const app = require("./app");
 const port = 3333;
 
-server.use(morgan("common"));
-
-server.listen(`${port}`, err => {
+app.listen(`${port}`, err => {
   if (err) {
     console.log("Aconteceu um erro...", err);
   } else {
