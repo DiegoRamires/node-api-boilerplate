c2a1356695170b77936068557430700cb7e77537
DiegoRamires <goncalves.ramires@gmail.com>
2020-03-25 22:24:55 -0300

    refactor app server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
+require('dotenv/config');
 const app = require('./app');
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(`${port}`, err => {
   if (err) {
-    console.log('Aconteceu um erro...', err);
+    console.log('Something went wrong...', err);
   } else {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`The server is running on PORT ${port}`);
   }
 });
